61cb7deed188c6045b419cb0fe75368ec329534b
Edward Serna <edward.serna@my.utsa.edu>
2026-06-04 11:10:51 -0500

    daily

---
+++ Dailies/6_4_26/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int totalWaviness(int num1, int num2) {
+
+    }
+};
+
+int main() {
+    Solution sol;
+
+    // Example 1: num1 = 120, num2 = 130 → expected: 3
+    int num1_1 = 120; int num2_1 =  130;
+    cout << "Input: " << num1_1 << ", " << num2_1 << endl;
+    cout << "\nOutput: " << sol.totalWaviness(num1_1, num2_1) <<endl<<endl;
+
+    return 0;
+}
