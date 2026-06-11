d1a821b88520685945abe2425320ad4a975d6c59
Edward Serna <edward.serna@my.utsa.edu>
2026-06-11 13:39:20 -0500

    daily

--- Dailies/6_11_26/main.cpp
+++ Dailies/6_11_26/main.cpp
@@ -4,8 +4,29 @@ using namespace std;
 
 class Solution {
 public:
+    static constexpr int MOD = 1e9 + 7;
+    static int dfs(vector<vector<int>>& adj, const int node, const int par) {
+        int depth = 0;
+        for (const auto &val : adj[node]) {
+            if (val == par) continue;
+            depth = max(depth, 1 + dfs(adj, val, node));
+        }
+        return depth;
+    }
+
     int assignEdgeWeights(vector<vector<int>>& edges) {
-        return 0;
+        vector<vector<int>> adjacentNodes((edges.size() + 1) + 1);
+        for (auto &edge : edges) { // Assign weights for edges
+            int top = edge[0];           int bottom = edge[1];
+            adjacentNodes[top].push_back(bottom);    adjacentNodes[bottom].push_back(top);
+        }
+
+        const int depth = dfs(adjacentNodes, 1, -1); // Finds depth of graph
+        long long result = 1;
+        for (int i = 1; i < depth; i++) {
+            result = (result * 2) % MOD;
+        }
+        return result;
     }
 };
 
@@ -36,7 +57,7 @@ int main() {
     // Example 2: edges = [[1,2],[1,3],[3,4],[3,5]] → expected: 2
     vector<vector<int>> nums2 = {{1,2}, {1,3}, {3,4}, {3,5}};
     cout << "Input: "; printArray(nums2);
-    cout << "\nOutput: "<< sol.assignEdgeWeights(nums2) <<endl<<endl;
+    cout << "\nOutput: "<< sol.assignEdgeWeights(nums2);
 
     return 0;
 }
